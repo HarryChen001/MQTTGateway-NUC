9e08016ed674191db0ca9c90d10178d21b57ec0b
ccl <ccl@ubuntu>
2019-07-08 11:09:19 +0800

    库文件优化

--- LIB/arm-linux/libaliyun/bak/include/infra_config.h
+++ LIB/arm-linux/libaliyun/bak/include/infra_config.h
@@ -7,6 +7,7 @@
 #define INFRA_STRING
 #define INFRA_NET
 #define INFRA_LIST
+#define INFRA_LOG_NETWORK_PAYLOAD
 #define INFRA_LOG
 #define INFRA_LOG_ALL_MUTED
 #define INFRA_LOG_MUTE_FLW
@@ -17,6 +18,7 @@
 #define INFRA_LOG_MUTE_CRT
 #define INFRA_TIMER
 #define INFRA_CJSON
+#define INFRA_SHA1
 #define INFRA_SHA256
 #define INFRA_REPORT
 #define INFRA_COMPAT
@@ -28,5 +30,6 @@
 #define DEVICE_MODEL_CLASSIC
 #define LOG_REPORT_TO_CLOUD
 #define DEVICE_MODEL_ENABLED
+#define DEVICE_MODEL_GATEWAY
 
 #endif
